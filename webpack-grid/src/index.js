58d693e3356bef2a875b87435c9bf14e6d1723e3
Gaojinpeng8 <511663568@qq.com>
2020-04-10 21:49:00 +0800

    first commit

---
+++ webpack-grid/src/index.js
@@ -0,0 +1,23 @@
+import './index.less' // css 改为 less
+import test from './test';
+
+// function timeout(time) {
+//     return new Promise(resolve => {
+//       setTimeout(resolve, time)
+//     })
+//   }
+  
+//   if (module.hot) {
+//     module.hot.accept('./test.js', function() {
+//       test();
+//     })
+//   }
+
+//   test();
+//   async function asyncPrint(value, time) {
+//     await timeout(time)
+//     console.log(value)
+//   }
+  
+//   asyncPrint('hello,world', 5000)
+console.log(test);
